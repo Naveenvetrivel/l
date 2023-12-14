de25a9f886c83bb33a45e61cbbb9167f22414210
Naveenvetrivel 21500178 <94165322+Naveenvetrivel@users.noreply.github.com>
2023-12-14 12:43:28 +0530

    Add files via upload

---
+++ Assignment 01/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            int n;
+            Console.WriteLine("Enter the Number of Rows : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write((char)(j + 64));
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
