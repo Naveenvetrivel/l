234c7d043489d23816f3a5f2dd1c022ebb9ceb87
Naveenvetrivel 21500178 <94165322+Naveenvetrivel@users.noreply.github.com>
2023-12-14 12:37:08 +0530

    Add files via upload

---
+++ Ex _01/Expno01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Expno01
+{
+    class Eligibility
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int math, phy, chem;
+            Console.Write("Enter your name:");
+            name = Console.ReadLine();
+            Console.Write("Enter Maths Mark:");
+            math = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Physics Mark:");
+            phy = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Chemistry Mark:");
+            chem = Convert.ToInt32(Console.ReadLine());
+            int tot1 = math + phy + chem;
+            int tot2 = math + phy;
+            if (math >= 65 && phy >= 55 && chem >= 50)
+            {
+                if (tot1 >= 180 || tot2 >= 140)
+                {
+                    Console.WriteLine(name + " is eligible for admission to an engineering course.");
+                }
+                else
+                {
+                    Console.WriteLine(name + " is not eligible for admission to an engineering course.");
+                }
+            }
+            else
+            {
+                Console.WriteLine(name + " is not eligible for admission to an engineering course.");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Ex_02/Ex_02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, revs = "";
+            Console.WriteLine("Enter string:");
+            s = Console.ReadLine();
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                revs += s[i].ToString();
+            }
+            if (revs == s)
+            {
+                Console.WriteLine("The entered string is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("The entered string is not Palindrome");
+            }
+        }
+    }
+}
